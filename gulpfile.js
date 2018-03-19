3e2079820f595142e51743830d31821a6455bf86
Dawid Rusnak <dawid@drcode.pl>
2018-03-19 12:26:55 +0100

    Prepare Gulp task to generate Vector Drawables for Android

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp')
 const svgo = require('gulp-svgo')
 
+const createSvgToVectorDrawableStream = require('./lib/createSvgToVectorDrawableStream')
+
 gulp.task('optimize-svg', () => {
   return gulp.src('icons/svg/**/*.svg')
     .pipe(svgo())
     .pipe(gulp.dest('icons/svg'))
+})
+
+gulp.task('android', () => {
+  return gulp.src('icons/svg/**/*.svg')
+    .pipe(createSvgToVectorDrawableStream())
+    .pipe(gulp.dest('icons/android'))
 })
